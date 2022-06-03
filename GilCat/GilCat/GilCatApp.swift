b04fafa6df41dc9fe0d86e2c1e49827b0e74bab4
wody-d <maxwodyd98@gmail.com>
2022-06-03 16:22:06 +0900

    feat: 폴더링 구조 생성
    - 레퍼런스 참고

--- GilCat/GilCat/GilCatApp.swift
+++ GilCat/GilCat/GilCatApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GilCatApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TestHomeView()
         }
     }
 }
---
+++ GilCat/GilCat/Sources/Global/Extensions/TestExtension.swift
@@ -0,0 +1,8 @@
+//
+//  TestExtension.swift
+//  GilCat
+//
+//  Created by Woody on 2022/06/03.
+//
+
+import Foundation
