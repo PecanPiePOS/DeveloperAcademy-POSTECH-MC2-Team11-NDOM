d226b9b5677dea85073c5dcf62d0b86661d5fa47
wody-d <maxwodyd98@gmail.com>
2022-06-03 15:43:04 +0900

    :tada: 프로젝트 생성

---
+++ GilCat/GilCat/GilCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GilCatApp.swift
+//  GilCat
+//
+//  Created by Woody on 2022/06/03.
+//
+
+import SwiftUI
+
+@main
+struct GilCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
