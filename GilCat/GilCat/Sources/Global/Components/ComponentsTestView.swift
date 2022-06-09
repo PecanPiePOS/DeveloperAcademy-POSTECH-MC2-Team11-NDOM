91b2e0ca4b4ae30c3206b2d33849bddb66618332
PecanPie / Kyubo Shim <hidra0321@naver.com>
2022-06-09 16:51:53 +0900

    feat: Half Modal Package 추가, 색상 에셋 추가
    half modal library 를 사용하기 위해 Package 추가 했으며, 색상 에셋을 추가하였음 + 츄르 이미지도 추가

--- GilCat/GilCat/Sources/Global/Components/ComponentsTestView.swift
+++ GilCat/GilCat/Sources/Global/Components/ComponentsTestView.swift
@@ -6,15 +6,3 @@
 //
 
 import SwiftUI
-
-struct ComponentsTestView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct ComponentsTestView_Previews: PreviewProvider {
-    static var previews: some View {
-        ComponentsTestView()
-    }
-}
