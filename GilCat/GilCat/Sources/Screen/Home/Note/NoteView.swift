51a909ad057c32a79bffb6f4932a19b47c11dc6c
PecanPie / Kyubo Shim <hidra0321@naver.com>
2022-06-09 20:11:56 +0900

    feat: 로그인 뷰 UI 만 추가, Package 삭제
    #14
    우선 Package 는 다음에 하고 library 가 필요없는 로그인 뷰 먼저 푸쉬.
    아직 서버 데이터는 연결하지 않아서 dummy data 만 넣고 UI 만 만듬

---
+++ GilCat/GilCat/Sources/Screen/Home/Note/NoteView.swift
@@ -0,0 +1,20 @@
+//
+//  NoteView.swift
+//  GilCat
+//
+//  Created by KYUBO A. SHIM on 2022/06/09.
+//
+
+import SwiftUI
+
+struct NoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoteView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteView()
+    }
+}
