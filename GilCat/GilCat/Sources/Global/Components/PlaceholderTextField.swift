301623931aa4c5c482db97b97ade876e01b5d96e
PecanPie / Kyubo Shim <hidra0321@naver.com>
2022-06-09 11:37:36 +0900

    feat: 텍스트필드, 네비게이션바 커스텀
    Global/Components 에 TextField placeholder 색상 변경 / 네비게이션 바의 색상 변경 func 추가

---
+++ GilCat/GilCat/Sources/Global/Components/PlaceholderTextField.swift
@@ -0,0 +1,36 @@
+//
+//  PlaceholderTextField.swift
+//  GilCat
+//
+//  Created by KYUBO A. SHIM on 2022/06/09.
+//
+
+import Foundation
+import SwiftUI
+
+// MARK: Placeholder color change modifier
+public struct PlaceholderStyle: ViewModifier {
+    var showPlaceHolder: Bool
+    var placeholder: String
+    
+    public func body(content: Content) -> some View {
+        ZStack(alignment: .leading) {
+            if showPlaceHolder {
+                Text(placeholder)
+                    .padding(.horizontal, 15)
+            }
+            content
+                .foregroundColor(Color.white)
+                .padding(5)
+        }
+    }
+}
+/*
+ * 사용 방법:
+ 
+ TextField("", text: $nickName)
+     .modifier(PlaceholderStyle(showPlaceHolder: nickName.isEmpty, placeholder: "원하는 닉네임을 정해주세요!"))
+     .foregroundColor(Color.white)
+ 
+ 이렇게 붙여야함, .foregroundColor 에서 원하는 색으로 바꾸면 됨
+ */
