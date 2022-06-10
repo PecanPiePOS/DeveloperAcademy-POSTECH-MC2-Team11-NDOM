1ca1c6c754cca81fca58e19a196e2d996f7ef5b2
ddophi98 <lightcloud98@gmail.com>
2022-06-10 22:01:51 +0900

    feat: 고양이 정보를 저장하는 구조체 생성

---
+++ GilCat/GilCat/Sources/Global/Components/CatInfo.swift
@@ -0,0 +1,15 @@
+//
+//  CatInfo.swift
+//  GilCat
+//
+//  Created by 김동락 on 2022/06/10.
+//
+
+struct CatInfo {
+    var name: String
+    var age: Int
+    var gender: String
+    var neutralized: Bool
+    var type: String
+    var imageName: String
+}
