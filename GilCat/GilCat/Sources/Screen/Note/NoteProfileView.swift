2398be493800369a62d6415fed140e98302ef360
PecanPie / Kyubo Shim <hidra0321@naver.com>
2022-06-12 12:37:17 +0900

    feat: Note 뷰 추가 +하프모달 패키지 추가
    노트뷰의 하위 뷰인 food, water, profile 뷰는 다음 푸쉬때 추가
    #17

---
+++ GilCat/GilCat/Sources/Screen/Note/NoteProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  NoteProfileView.swift
+//  GilCat
+//
+//  Created by KYUBO A. SHIM on 2022/06/12.
+//
+
+import SwiftUI
+
+struct NoteProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoteProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteProfileView()
+    }
+}
