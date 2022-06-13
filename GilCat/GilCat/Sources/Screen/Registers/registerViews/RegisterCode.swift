e7cae62d5acd2a08b26f80ee7707567b3a4aa257
Hardy <dusgh15378@gmail.com>
2022-06-13 22:33:37 +0900

    fix: 네비게이션 타이틀UI 작업 적용 안된 부분 수정 후 재 커밋

--- GilCat/GilCat/Sources/Screen/Registers/registerViews/RegisterCode.swift
+++ GilCat/GilCat/Sources/Screen/Registers/registerViews/RegisterCode.swift
@@ -14,78 +14,95 @@ struct RegisterCode: View {
     @State var isLinkActive = false
     @State var isAlertActice = false
     @State var codeInput = ""
+    @Environment(\.presentationMode) var presentation
     
     init(_ buildNavigationStack: Binding<Bool>) {
         UITextView.appearance().backgroundColor = .clear
         self._buildNavigationStack = buildNavigationStack
+        Theme.navigationBarColors(background: .systemFill, titleColor: .white)
     }
     
     var body: some View {
-        VStack {
-            // 설명
-            CustomSubTitle(text: "※ 다른 사람과 공유할 시, 개인 메모를 제외한 이전 기록이 모두 공유됩니다. ")
+        ZStack {
+            VStack {
+                // 설명
+                CustomSubTitle(text: "※ 다른 사람과 공유할 시, 개인 메모를 제외한 이전 기록이 모두 공유됩니다. ")
+                    .padding()
+                // 제목
+                HStack {
+                    GilCatTitle(titleText: "공유 코드").padding([.top, .leading])
+                    Spacer()
+                }
+                // 코드 입력 칸
+                ZStack {
+                    TextField("", text: $codeInput)
+                        .frame(width: 0, height: 0)
+                        .focused($isFocused, equals: true)
+                        .keyboardType(.numberPad)
+                        .onChange(of: codeInput) { _ in
+                            if codeInput.count > 6 {
+                                codeInput = String(codeInput.prefix(6))
+                            }
+                        }
+                    HStack {
+                        getCodeInputView(index: 0)
+                        getCodeInputView(index: 1)
+                        getCodeInputView(index: 2)
+                        getCodeInputView(index: 3)
+                        getCodeInputView(index: 4)
+                        getCodeInputView(index: 5)
+                    }
+                }
                 .padding()
-            // 제목
-            HStack {
-                GilCatTitle(titleText: "공유 코드").padding([.top, .leading])
                 Spacer()
-            }
-            // 코드 입력 칸
-            ZStack {
-                TextField("", text: $codeInput)
-                    .frame(width: 0, height: 0)
-                    .focused($isFocused, equals: true)
-                    .keyboardType(.numberPad)
-                    .onChange(of: codeInput) { _ in
-                        if codeInput.count > 6 {
-                            codeInput = String(codeInput.prefix(6))
-                        }
-                    }
-                HStack {
-                    getCodeInputView(index: 0)
-                    getCodeInputView(index: 1)
-                    getCodeInputView(index: 2)
-                    getCodeInputView(index: 3)
-                    getCodeInputView(index: 4)
-                    getCodeInputView(index: 5)
-                }
-            }
-            .padding()
-            Spacer()
-            // 메인 버튼
-            NavigationLink(destination: RegisterName($buildNavigationStack), isActive: $isLinkActive) {
-                HStack {
-                    Button {
-                        isLinkActive = true
-                    } label: {
-                        GilCatMainButton(text: "건너뛰기", foreground: .white, background: .pickerColor)
-                    }
-                    Button {
-                        // Todo: 코드에 따라 서버에서 다른 고양이 룸 정보 받아오기
-                        // 코드가 다 입력이 안됐다면, 팝업 창 보여주기
-                        if codeInput.count != 6 {
-                            isAlertActice = true
-                        } else {
+                // 메인 버튼
+                NavigationLink(destination: RegisterName($buildNavigationStack), isActive: $isLinkActive) {
+                    HStack {
+                        Button {
                             isLinkActive = true
+                        } label: {
+                            GilCatMainButton(text: "건너뛰기", foreground: .white, background: .pickerColor)
                         }
-                    } label: {
-                        GilCatMainButton(text: "다음", foreground: .white, background: .buttonColor)
+                        Button {
+                            // Todo: 코드에 따라 서버에서 다른 고양이 룸 정보 받아오기
+                            // 코드가 다 입력이 안됐다면, 팝업 창 보여주기
+                            if codeInput.count != 6 {
+                                isAlertActice = true
+                            } else {
+                                isLinkActive = true
+                            }
+                        } label: {
+                            GilCatMainButton(text: "다음", foreground: .white, background: .buttonColor)
+                        }
                     }
+                    .padding()
                 }
-                .padding()
+                .isDetailLink(false)
             }
-            .isDetailLink(false)
-        }
-        .navigationBarTitle("코드", displayMode: .inline)
-        .background(Color.backgroundColor)
-        .alert("코드를 모두 입력하거나 건너뛰기를 눌러주세요", isPresented: $isAlertActice) {
-            Button("확인") {}
-        }
-        .onAppear {
-            // 화면이 나타나고 0.5초 뒤에 자동으로 공유코드 첫번째 입력칸에 포커스 되도록 하기
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    isFocused = true
+            .navigationTitle("공유코드")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
+            .navigationViewStyle(.stack)
+            // MARK: 툴바 수정
+            .toolbar {
+                ToolbarItem(placement: .navigation) {
+                    Image(systemName: "chevron.backward")
+                        .foregroundColor(.white)
+                        .onTapGesture {
+                            self.presentation.wrappedValue.dismiss()
+                        }
+                }
             }
+            .background(Color.backgroundColor)
+            .alert("코드를 모두 입력하거나 건너뛰기를 눌러주세요", isPresented: $isAlertActice) {
+                Button("확인") {}
+            }
+            .onAppear {
+                // 화면이 나타나고 0.5초 뒤에 자동으로 공유코드 첫번째 입력칸에 포커스 되도록 하기
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        isFocused = true
+                }
+        }
         }
     }
     
--- GilCat/GilCat/Sources/Screen/Registers/registerViews/RegisterStart.swift
+++ GilCat/GilCat/Sources/Screen/Registers/registerViews/RegisterStart.swift
@@ -10,42 +10,60 @@ import SwiftUI
 struct RegisterStart: View {
     @State var isLinkActive = false
     @EnvironmentObject var catInfo: GilCatInfoList
+    @Environment(\.presentationMode) var presentation
     //    @Binding var buildNavigationStack: Bool
+    init(){Theme.navigationBarColors(background: .systemFill, titleColor: .white)}
+    
     var body: some View {
         // Todo: 현재는 네비게이션뷰로 다른 뷰들을 연결했지만 괜찮은 애니메이션 전환 요소를 찾으면 바꿀 예정입니다.
         NavigationView {
-            VStack {
-                // 제목
-                HStack {
-                    GilCatTitle(titleText: "나만의 길고양이 기록장을 만들어보세요!").padding([.top, .leading])
-                    Spacer()
-                }
-                // 목차
-                VStack(alignment: .leading) {
-                    getProcessContentView(order: 1, text: "공유 코드가 있다면 알려주세요!")
-                    getProcessContentView(order: 2, text: "길냥이 프로필을 적어주세요!")
-                    getProcessContentView(order: 3, text: "나만의 길냥이를 만들어주세요!")
-                }
-                .padding()
-                Spacer()
-                // 메인 버튼
-                NavigationLink(destination: RegisterCode($isLinkActive), isActive: $isLinkActive) {
-                    Button {
-                        isLinkActive = true
-                        // 고양이 정보를 저장하는 새로운 객체 생성
-                        // 등록하다가 뒤로 가기로 다시 왔다면 객체 생성을 하지 않고 기존 객체를 씀
-                        if catInfo.infoList.isEmpty || catInfo.infoList[catInfo.infoList.endIndex-1].isUploadedToServer {
-                            catInfo.infoList.append(GilCatInfo())
-                        }
-                    } label: {
-                        GilCatMainButton(text: "시작하기", foreground: Color.white, background: .buttonColor)
+            ZStack {
+                VStack {
+                    // 제목
+                    HStack {
+                        GilCatTitle(titleText: "나만의 길고양이 기록장을 만들어보세요!").padding([.top, .leading])
+                        Spacer()
+                    }
+                    // 목차
+                    VStack(alignment: .leading) {
+                        getProcessContentView(order: 1, text: "공유 코드가 있다면 알려주세요!")
+                        getProcessContentView(order: 2, text: "길냥이 프로필을 적어주세요!")
+                        getProcessContentView(order: 3, text: "나만의 길냥이를 만들어주세요!")
                     }
                     .padding()
+                    Spacer()
+                    // 메인 버튼
+                    NavigationLink(destination: RegisterCode($isLinkActive), isActive: $isLinkActive) {
+                        Button {
+                            isLinkActive = true
+                            // 고양이 정보를 저장하는 새로운 객체 생성
+                            // 등록하다가 뒤로 가기로 다시 왔다면 객체 생성을 하지 않고 기존 객체를 씀
+                            if catInfo.infoList.isEmpty || catInfo.infoList[catInfo.infoList.endIndex-1].isUploadedToServer {
+                                catInfo.infoList.append(GilCatInfo())
+                            }
+                        } label: {
+                            GilCatMainButton(text: "시작하기", foreground: Color.white, background: .buttonColor)
+                        }
+                        .padding()
+                    }
+                    .isDetailLink(false)
                 }
-                .isDetailLink(false)
+                .background(Color.backgroundColor)
+            .navigationTitle("목차")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
+            .navigationViewStyle(.stack)
+            // MARK: 툴바 수정
+            .toolbar {
+                ToolbarItem(placement: .navigation) {
+                    Image(systemName: "chevron.backward")
+                        .foregroundColor(.white)
+                        .onTapGesture {
+                            self.presentation.wrappedValue.dismiss()
+                        }
+                }
             }
-            .background(Color.backgroundColor)
-            .navigationBarTitle("목차", displayMode: .inline)
+            }
         }
     }
     
