380ad3f6c8235aacbd7e76ef297d71ac5672d312
Hardy <dusgh15378@gmail.com>
2022-06-13 09:34:23 +0900

    refactor : comflict오류 해결작업

---
+++ GilCat/GilCat/Sources/Screen/Registers/registerViews/RegisterType.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterType.swift
+//  GilCat
+//
+//  Created by 김연호 on 2022/06/13.
+//
+
+import SwiftUI
+
+struct RegisterType: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegisterType_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterType()
+    }
+}
