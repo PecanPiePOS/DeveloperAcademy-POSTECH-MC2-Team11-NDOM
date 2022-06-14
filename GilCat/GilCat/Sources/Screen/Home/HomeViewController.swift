db1c4793701e76ddc54779c163aca57c78ae88b6
wody-d <maxwodyd98@gmail.com>
2022-06-14 15:39:06 +0900

    feat: HomeViewController, HomeStoryboard 추가

---
+++ GilCat/GilCat/Sources/Screen/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  GilCat
+//
+//  Created by Woody on 2022/06/14.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
