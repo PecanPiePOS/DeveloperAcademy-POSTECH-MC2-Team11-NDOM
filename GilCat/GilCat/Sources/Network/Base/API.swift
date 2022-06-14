0cfb9fc7406c794cc72f9c11b499ba119ab431f8
wody-d <maxwodyd98@gmail.com>
2022-06-14 09:49:27 +0900

    feat: API 기본 URL 추가

---
+++ GilCat/GilCat/Sources/Network/Base/API.swift
@@ -0,0 +1,26 @@
+//
+//  API.swift
+//  GilCat
+//
+//  Created by Woody on 2022/06/14.
+//
+
+import Foundation
+import Moya
+
+enum API {
+    case mock
+}
+
+extension TargetType {
+    var baseURL: URL {
+        URL(string: "https://ebad3845-c82e-4c17-8284-107365ea3e67.mock.pstmn.io")!
+    }
+    
+    var headers: [String: String]? {
+        [
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " // TODO: 유저 정보를 저장해두어야함 (토큰 정보)
+        ]
+    }
+}
